461f323facbe68e64f16188d7bf2a0758f4857f9
rick1128 <111245831+rick1128@users.noreply.github.com>
2023-01-14 17:24:40 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,18 @@
 from telethon.sync import TelegramClient
 from telethon.sessions import StringSession
 import os
-APP_ID = os.environ.get("APP_ID")
-APP_HASH = os.environ.get("APP_HASH")
-BOT_USERNAME = os.environ.get("BOT_USERNAME")
-session = os.environ.get("TERMUX")
-SESSION = os.environ.get("TERMUX")
-token = os.environ.get("TOKEN")
+APP_ID = os.environ.get("APP_ID",
+"اب ايديك")
+APP_HASH = os.environ.get("APP_HASH",
+"هاشك")
+BOT_USERNAME = os.environ.get("BOT_USERNAME",
+                           "يوزر بوتك بدون @")
+session = os.environ.get("TERMUX",
+                        "تيرمكسك")
+SESSION = os.environ.get("TERMUX",
+                        "تيرمكسك للمره الثانيه")
+token = os.environ.get("TOKEN",
+                      "توكنك")
 sedthon = TelegramClient(StringSession(session), APP_ID, APP_HASH)
 bot = TelegramClient("bot", APP_ID, APP_HASH).start(bot_token=token)
 ispay = ['yes']
